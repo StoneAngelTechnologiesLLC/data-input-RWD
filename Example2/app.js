2aac7f725eed03aee6d85ccff4183737376818be
Pietrangelo <stoneangeltechnologies@gmail.com>
2017-02-17 08:29:38 -0700

    version 3

--- Example2/app.js
+++ Example2/app.js
@@ -1,1 +1,92 @@
-// app
+// Created By: John Joseph Pietrangelo III 					GO DEVILS!!
+
+// *** Uncomment the lines 8 thru 12 as well as line 62, 85 & 86 below to begin Phases 1-6b ***	
+//  Uncomment each Phase, in order. Open HTML file in browser after uncommenting a phase to see result 
+
+// var main = 
+	
+// 	function()
+// 	{
+// 		"use strict";
+
+// 		$(".comments button").on("click",  function(event)
+												//				Phase# 1 
+											    // {
+											   	// 		console.log("hello World!");
+											   	// }
+											   	   //				Phase# 2
+											   	   // {
+											   	   // 		$(".comments-input").append("<p>This is a new comment<p/>");
+											   	   // }
+											   	      //        		Phase# 3
+											   	      // {
+											   	      // 		var $new_comment = $("<p>");
+											   	      // 		$new_comment.text("This is a new comment");
+											   	      // 		
+											   	      //        $(".comments-input").append($new_comment);
+											   	      // }
+											   	         //					Phase# 4
+											   	         // {
+											   	         // 	var $new_comment = $("<p>").text("This is a new comment");
+											   	      	
+											   	         // 	$(".comments-input").append($new_comment);
+											   	         // }
+											   	    //					Phase# 5 	   
+											   		// {
+											   		// 	var $new_comment = $("<p>"),
+											   			
+											   		// 	comment_text = $(".comments input").val();
+
+											   		// 	$new_comment.text(comment_text);
+
+											   		// 	$(".comments-input").append($new_comment);
+											   		// }
+
+											   		//				Phase# 6-a
+		      //                                       {
+								// 			   			var $new_comment;
+											 
+								// 			   			if ($(".comments input").val() !== "") 
+								// 			   			{
+								// 			   				$new_comment = $("<p>").text($(".comments input").val());
+								// 			   				$new_comment.hide();
+								// 			   				$(".comments-input").append($new_comment);
+								// 			   				//Defualt parameter value is 400/ms: 
+								// 							//Either value, number (500) or string ("500") data-type will be accepted.
+								// 							$new_comment.fadeIn(1000);
+								// 			   				$(".comments input").val("");
+								// 			   			}	
+								// 	
+
+				// *** Uncomment the line below for Phases 1-6b ***				   		}
+								// );
+
+	//				Phase# 6-b
+		// $(".comments input").on("keypress", function (event) 
+		// {
+		// 	var $new_comment;
+
+		// 	if (event.keyCode === 13)
+		// 	{
+		// 		if ($(".comments input").val() !== "") 
+		// 		{
+		// 			$new_comment = $("<p>").text($(".comments input").val());
+		// 			$new_comment.hide();
+		// 			$(".comments-input").append($new_comment);
+		// 			//Defualt parameter value is 400/ms: 
+		// 			//Either value, number (500) or string ("500") data-type will be accepted.
+		// 			$new_comment.fadeIn(1000);
+		// 			$(".comments input").val("");
+		// 		}	
+
+		// 	}
+		// });
+     // *** Uncomment the 2 lines below for Phases 1-6b ***	       
+ //    };      																								
+	// $(document).ready(main);
+
+
+	// 						***	FINAL Phase #7 ***
+
+	
+
